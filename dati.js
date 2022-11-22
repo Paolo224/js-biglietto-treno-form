624ff88dabf4def8de1e35a47d9df1838a0cd1f6
Paolo224 <picariellopaolo224@gmail.com>
2022-11-22 15:49:03 +0100

    inizializzazione dell'if

--- dati.js
+++ dati.js
@@ -1,11 +1,20 @@
-const distanzaInKm = document.querySelector ('input#km-distanza');
+const distanzaInKm = document.querySelector ('#km-distanza');
 
-const etaDelPasseggero = document.querySelector ('input#eta');
+const etaDelPasseggero = document.querySelector ('#eta');
 
-const button = document.querySelector ('btn');
+const button = document.querySelector ('.btn');
+
+let prezzo = distanzaInKm * 0.2762;
+
+let output;
 
 button.addEventListener('click', function() {
     console.log(etaDelPasseggero.value);
     console.log(distanzaInKm.value);
+    
+    if ((etaDelPasseggero > 18) && (etaDelPasseggero < 65)){
+        console.log(prezzo);
+    } else if (etaDelPasseggero < 18)
+        console.log()
 })
 
