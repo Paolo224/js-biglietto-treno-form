d1f15fde7928a64ac70348f46058df1c664ec6bf
Paolo224 <picariellopaolo224@gmail.com>
2022-11-22 16:15:03 +0100

    Parte js funzionante

--- dati.js
+++ dati.js
@@ -4,17 +4,29 @@ const etaDelPasseggero = document.querySelector ('#eta');
 
 const button = document.querySelector ('.btn');
 
-let prezzo = distanzaInKm * 0.2762;
+let prezzoMinorenni;
 
-let output;
+let prezzoAnziani;
+
+let prezzo;
+
+let prezzoFinale;
 
 button.addEventListener('click', function() {
+    prezzo = distanzaInKm.value * 0.2762;
+    prezzoAnziani = prezzo * 33.3 /100;
+    prezzoMinorenni = prezzo * 12.5 / 100;
     console.log(etaDelPasseggero.value);
     console.log(distanzaInKm.value);
     
-    if ((etaDelPasseggero > 18) && (etaDelPasseggero < 65)){
-        console.log(prezzo);
-    } else if (etaDelPasseggero < 18)
-        console.log()
+    if ((etaDelPasseggero.value > 18) && (etaDelPasseggero.value < 65)){
+        prezzoFinale = prezzo;
+    } else if (etaDelPasseggero.value < 18){
+        prezzoFinale = (prezzo - prezzoMinorenni);
+    } else {
+        prezzoFinale = (prezzo - prezzoAnziani);
+    }
+
+    document.getElementById('output').innerHTML = prezzoFinale;
 })
 
